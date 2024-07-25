b97ad4062b9abf3ac5c558d0c23eb9ae9a9818ae
AbdoAnany <abdo10002011@gmail.com>
2024-07-25 19:53:51 +0300

    auth clean architecture

---
+++ lib/features/2_auth/domain/entities/user_entity.dart
@@ -0,0 +1,27 @@
+class UserEntity {
+  final String id;
+  final String email;
+  final String role;
+
+  UserEntity({
+    required this.id,
+    required this.email,
+    required this.role,
+  });
+  // Optionally, you can add methods to serialize/deserialize the object
+  factory UserEntity.fromJson(Map<String, dynamic> json) {
+    return UserEntity(
+      id: json['id'],
+      email: json['email'],
+      role: json['role'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'email': email,
+      'role': role,
+    };
+  }
+}
