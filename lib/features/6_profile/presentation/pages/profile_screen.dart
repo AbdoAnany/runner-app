2e6ab5af9aa315aa9e415ab4c633184198d42fb2
a.anany <abdo10002011@gmail.com>
2024-10-27 18:13:05 +0300

    add features/auth clean

--- lib/features/6_profile/presentation/pages/profile_screen.dart
+++ lib/features/6_profile/presentation/pages/profile_screen.dart
@@ -6,6 +6,7 @@ import 'package:runner_app/core/helper/extension.dart';
 import 'package:runner_app/dependency_injection.dart';
 
 import '../../../../core/widgets/loading_widget.dart';
+import '../../../../my_app.dart';
 import '../../../2_auth/presentation/manager/auth/auth_bloc.dart';
 import '../widgets/header_profile.dart';
 import '../widgets/profile_body_list.dart';
